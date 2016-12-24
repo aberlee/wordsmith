dc8406b816cc22f55660c67be192f1507fbd200d
Rena <renashinomiya@gmail.com>
2016-12-24 15:59:39 -0600

    Add standard keyboard keys header

---
+++ src/keyboard.h
@@ -0,0 +1,42 @@
+/**********************************************************//**
+ * @file keyboard.h
+ * @author Wes64
+ * @brief Defines the standard keys we care about.
+ **************************************************************/
+
+#ifndef _KEYBOARD_H_
+#define _KEYBOARD_H_
+
+// Allegro
+#include <allegro5/allegro.h>
+
+/*============================================================*
+ * Key macros
+ *============================================================*/
+
+/// The up key
+#define KEY_UP ALLEGRO_KEY_UP
+
+/// The down key
+#define KEY_DOWN ALLEGRO_KEY_DOWN
+
+/// The left key
+#define KEY_LEFT ALLEGRO_KEY_LEFT
+
+/// The right key
+#define KEY_RIGHT ALLEGRO_KEY_RIGHT
+
+/// The "Start" key
+#define KEY_START ALLEGRO_KEY_W
+
+/// The "Select" key
+#define KEY_SELECT ALLEGRO_KEY_A
+
+/// The "A" key
+#define KEY_A ALLEGRO_KEY_D
+
+/// The "B" key
+#define KEY_B ALLEGRO_KEY_S
+
+/*============================================================*/
+#endif // _KEYBOARD_H_
