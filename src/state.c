3370708d3515fcb2542b53a9f1fc6603727e8298
Rena <renashinomiya@gmail.com>
2016-12-24 15:59:16 -0600

    Add standard STATE interface

---
+++ src/state.c
@@ -0,0 +1,61 @@
+/**********************************************************//**
+ * @file state.c
+ * @author Wes64
+ * @brief Implements game state interface.
+ **************************************************************/
+
+// This project
+#include "state.h"
+
+/*============================================================*
+ * State stack
+ *============================================================*/
+#define STATE_STACK_SIZE 64             // Size of state stack
+static STATE stack[STATE_STACK_SIZE];   // The state stack.
+int current = -1;                       // Top of the state stack.
+
+/*============================================================*
+ * Changing to a new state
+ *============================================================*/
+bool state_Push(const STATE *state) {
+    if (current+1 < STATE_STACK_SIZE) {
+        current++;
+        stack[current] = *state;
+        return true;
+    }
+    return false;
+}
+
+/*============================================================*
+ * Return to a previous state
+ *============================================================*/
+bool state_Pop(void) {
+    if (current >= 0) {
+        current--;
+        return true;
+    }
+    return false;
+}
+
+/*============================================================*
+ * Draw the current state
+ *============================================================*/
+bool state_Draw(void) {
+    if (current >= 0) {
+        stack[current].draw();
+        return true;
+    }
+    return false;
+}
+
+/*============================================================*
+ * Run the current state
+ *============================================================*/
+bool state_Run(const ALLEGRO_EVENT *event) {
+    if (current >= 0) {
+        return stack[current].run(event);
+    }
+    return false;
+}
+
+/*============================================================*/
---
+++ src/state.h
@@ -0,0 +1,52 @@
+/**********************************************************//**
+ * @file state.h
+ * @author Wes64
+ * @brief Defines a standard interface to game states.
+ **************************************************************/
+
+#ifndef _STATE_H_
+#define _STATE_H_
+
+// Standard library
+#include <stdbool.h>
+
+// Allegro
+#include <allegro5/allegro.h>
+
+/**********************************************************//**
+ * @struct STATE
+ * @brief Provides a set of functions that control the game
+ * state at any time.
+ **************************************************************/
+typedef struct {
+    void (*draw)(void);     ///< Function to draw the state.
+    bool (*run)(const ALLEGRO_EVENT *); ///< Runs the state.
+} STATE;
+
+/**********************************************************//**
+ * @brief Draws the current state.
+ * @return Whether the drawing succeeded.
+ **************************************************************/
+extern bool state_Draw(void);
+
+/**********************************************************//**
+ * @brief Runs one frame of the current state.
+ * @return Whether the run succeeded.
+ **************************************************************/
+extern bool state_Run(const ALLEGRO_EVENT *event);
+
+/**********************************************************//**
+ * @brief Changes to a new state.
+ * @param state: The next state. This can be deleted after.
+ * @return Whether the state change succeeded.
+ **************************************************************/
+extern bool state_Push(const STATE *state);
+
+/**********************************************************//**
+ * @brief Returns to the previous state.
+ * @return Whether the state change succeeded.
+ **************************************************************/
+extern bool state_Pop(void);
+
+/*============================================================*/
+#endif // _STATE_H_
