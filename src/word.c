36430cbd01194a5bd70b29b5fc37569b55512261
Rena <renashinomiya@gmail.com>
2016-12-25 20:00:15 -0600

    Add initial WORD implementation

---
+++ src/word.c
@@ -0,0 +1,304 @@
+/**********************************************************//**
+ * @file word.h
+ * @brief Wordsmith word implementations.
+ **************************************************************/
+
+// Standard library
+#include <stddef.h>     // size_t
+#include <stdbool.h>    // bool
+#include <string.h>     // strlen, strcpy
+
+// This project
+#include "debug.h"      // assert, eprintf
+#include "word.h"       // WORD
+#include "technique.h"  // TECHNIQUE
+
+/// Maps each letter (0-25) to the stat it's associated with.
+static const char LETTER_STATS[N_LETTERS] = {
+    MAXHP,  // A
+    DEFEND, // B
+    SPEED,  // C
+    DEFEND, // D
+    MAXHP,  // E
+    ATTACK, // F
+    DEFEND, // G
+    SPEED,  // H
+    MAXHP,  // I
+    ATTACK, // J
+    ATTACK, // K
+    SPEED,  // L
+    DEFEND, // M
+    SPEED,  // N
+    MAXHP,  // O
+    SPEED,  // P
+    SPEED,  // Q
+    ATTACK, // R
+    ATTACK, // S
+    DEFEND, // T
+    MAXHP,  // U
+    ATTACK, // V
+    DEFEND, // W
+    ATTACK, // X
+    MAXHP,  // Y
+    SPEED,  // Z
+};
+
+/**********************************************************//**
+ * @brief Maps each letter to a corresponding stat using the
+ * LETTER_STATS lookup table.
+ * @param letter: An uppercase or lowercase letter.
+ * @return The stat associated to the letter or -1 on failure.
+ **************************************************************/
+static STAT LetterStat(char letter) {
+    int index;
+
+    // Try lower case
+    index = letter - 'a';
+    if (index >= 0 && index < N_LETTERS) {
+        return LETTER_STATS[index];
+    }
+    
+    // Try upper case
+    index = letter - 'A';
+    if (index >= 0 && index < N_LETTERS) {
+        return LETTER_STATS[index];
+    }
+    
+    // Failed to get the stat
+    eprintf("Invalid word letter: '%c'\n", letter);
+    return -1;
+}
+
+/**********************************************************//**
+ * @brief Maps two stats to a unique stat codon.
+ * @param first: The primary stat.
+ * @param second: The secondary stat.
+ * @return A unique stat codon.
+ **************************************************************/
+static inline int StatCodon(STAT first, STAT second) {
+    // Generates a codon from the two stat keys
+    // Stat keys are 0, 1, 2, 3 (2 bits)
+    return (first << 2) | second;
+}
+
+// The total number of unique technique codons.
+#define N_CODONS (N_STATS*N_STATS)
+
+// Codon stacks
+#define PRIMARY 0   // This is the primary technique for the codon.
+#define ADVANCED 1  // This is the secondary technique for the codon.
+
+// Total number of codon stacks.
+#define N_STACKS 2
+
+// Base stat boost when stacks are too large.
+#define STAT_BOOST 2
+
+/// Mapping techniques from codons.
+static const TECHNIQUE TECHNIQUE_TABLE[N_CODONS][N_STACKS] = {
+    // Health-dominant techniques
+    {HEAL, RECOVER},        // HH
+    {DRAIN, ANTI_HEAL},     // HA
+    {CURE, AURA},           // HD
+    {EMERGENCY, TEAM_HEAL}, // HS
+    
+    // Attack-dominant techniques
+    {SMASH, EXPLOSION},     // AH
+    {CHARGE, BREAK},        // AA
+    {STUN, BLUNT},          // AD
+    {WRAP, SLOW},           // AS
+    
+    // Defend-dominant techniques
+    {PROTECT, SLOW_SWITCH}, // DH
+    {TANK, RETALIATE},      // DA
+    {BOLSTER, SCREEN},      // DD
+    {SLOW_ATTACK, REFLECT}, // DS
+    
+    // Speed-dominant attacks
+    {CONCENTRATE, STEAL},           // SH
+    {SWITCH_ATTACK, QUICK_ATTACK},  // SA
+    {HAZARD, EJECT},                // SD
+    {QUICKEN, SWIFT},               // SS
+};
+
+/**********************************************************//**
+ * @brief Gets the technique the codon is associated with.
+ * @param codon: The stat codon.
+ * @param stacks: Number of times the codon has occurred
+ * previously in the word.
+ * @return The technique or -1 on failure.
+ **************************************************************/
+static TECHNIQUE CodonTechnique(int codon, int stacks) {
+    
+    // Error checking
+    if (codon < 0 || codon >= N_CODONS) {
+        eprintf("Invalid codon: %d\n", codon);
+        return -1;
+    }
+    
+    // Get the technique from the table
+    switch (stacks) {
+    case PRIMARY:
+        return TECHNIQUE_TABLE[codon][PRIMARY];
+        
+    case ADVANCED:
+        return TECHNIQUE_TABLE[codon][PRIMARY];
+    
+    default:
+        // Too many stacks applied, no further techniques!
+        return NONE;
+    }
+}
+
+/**********************************************************//**
+ * @brief Recompute the word's stats.
+ * @param word: The word tp update.
+ **************************************************************/
+static inline void word_UpdateStats(WORD *word) {
+    for (int i = 0; i < N_STATS; i++) {
+        word->stat[i] = (word->base[i]*(word->level + 5)) / 100;
+    }
+}
+
+/**********************************************************//**
+ * @brief Letter location weighting function.
+ * @param location: Location of the letter.
+ * @return The base stat points allocated for the letter.
+ * This must be positive!
+ **************************************************************/
+static inline int WeightLocation(int location) {
+    return MAX_WORD_LENGTH - location;
+}
+
+/**********************************************************//**
+ * @brief Experience needed to move from level n to level n+1.
+ * @param n: The current level.
+ * @return The amount of experience needed.
+ **************************************************************/
+static inline int ExperienceNeeded(int n) {
+    return n*n;
+}
+
+/*============================================================*
+ * Creating a word
+ *============================================================*/
+bool word_Create(WORD *word, const char *text, int level) {
+    
+    // Set word text
+    int length = strlen(text);
+    if (length < MIN_WORD_LENGTH || length > MAX_WORD_LENGTH) {
+        eprintf("The word \"%s\" is of invalid length.\n", text);
+        return false;
+    }
+    strncpy(word->text, text, length);
+    word->text[length] = '\0';
+    
+    // Set level
+    if (level < MIN_LEVEL || level > MAX_LEVEL) {
+        eprintf("Invalid initial word level: %d\n", level);
+        return false;
+    }
+    word->level = level;
+    
+    // Accumulate total base stats for the letter
+    // The word gets points for each letter, with letters located
+    // earlier in the word having more weight. After this is
+    // completed, the word certainly contains only valid stats.
+    int acc[N_STATS] = {1, 1, 1, 1};
+    STAT stat;
+    for (int i = 0; i < length; i++) {
+        // Weight each letter according to its position in the word
+        if ((stat = LetterStat(text[i])) < 0) {
+            eprintf("Invalid stat discovered: %d\n", stat);
+            return false;
+        }
+        acc[stat] += WeightLocation(i);
+    }
+    
+    // Scale base stat totals (balancing)
+    int total = 0;
+    for (int i = 0; i < N_STATS; total += acc[i++]);
+    int statAverage = (total + 3) / 4;
+    
+    // Set base stats
+    int baseStat;
+    for (int i = 0; i < N_STATS; i++) {
+        // Multiply first to avoid truncation errors
+        baseStat = (acc[i] * 100) / statAverage;
+        if (baseStat > MAX_STAT) {
+            baseStat = MAX_STAT;
+        } else if (baseStat < MIN_STAT) {
+            baseStat = MIN_STAT;
+        }
+        word->base[i] = baseStat;
+    }
+    
+    // Find all techniques the word can learn
+    int codon;
+    int codonStacks[N_CODONS];
+    memset(codonStacks, PRIMARY, sizeof(codonStacks));
+    int first, second = LetterStat(text[0]);
+    int index;
+    TECHNIQUE tech;
+    word->nTechs = 0;
+    for (int i = 1; i < length && word->nTechs < MAX_TECHNIQUES; i++) {
+        // Read the next codon (loop if invalid)
+        first = second;
+        index = i % length;
+        second = LetterStat(text[index]);
+        codon = StatCodon(first, second);
+        if ((tech = CodonTechnique(codon, codonStacks[codon]++)) < 0) {
+            eprintf("Invalid technique: %d\n", tech);
+            return false;
+        }
+        
+        // Boost base stat if a repeat codon was discovered.
+        // It is OK if the stats boosted are the same.
+        // It is also impossible for tech to already exist in
+        // the word's moveset so long as the mapping array is
+        // configured properly.
+        if (tech == NONE) {
+            word->base[first] += STAT_BOOST;
+            word->base[second] += STAT_BOOST;
+        } else {
+            word->techs[word->nTechs++] = tech;
+        }
+    }
+    
+    // Initialize stats
+    word->exp = ExperienceNeeded(level);
+    word_UpdateStats(word);
+    word->hp = word->stat[MAXHP];
+    return true;
+}
+
+/*============================================================*
+ * Changing HP
+ *============================================================*/
+void word_ChangeCurrentHP(WORD *word, int delta) {
+    int temp = word->hp + delta;
+    int max = word->stat[MAXHP];
+    int min = 0;
+    if (temp > max) {
+        word->hp = max;
+    } else if (temp < min) {
+        word->hp = min;
+    } else {
+        word->hp = temp;
+    }
+    return;
+}
+
+/*============================================================*
+ * Gaining experience
+ *============================================================*/
+void word_ChangeExperience(WORD *word, int delta) {
+    word->exp -= delta;
+    while (word->exp < 0) {
+        word->level++;
+        word->exp += ExperienceNeeded(word->level);
+    }
+    word_UpdateStats(word);
+}
+
+/*============================================================*/
---
+++ src/word.h
@@ -0,0 +1,101 @@
+/**********************************************************//**
+ * @file word.h
+ * @brief Wordsmith word data header file.
+ **************************************************************/
+ 
+#ifndef _WORD_H_
+#define _WORD_H_
+
+// Standard library
+#include <stdbool.h>    // bool
+
+// This project
+#include "technique.h"  // TECHNIQUE
+
+/*============================================================*
+ * Constants
+ *============================================================*/
+
+/// Number of letters supported (a-z)
+#define N_LETTERS 26
+
+/// The maximum number of special techniques any word can have.
+#define MAX_TECHNIQUES 4
+
+// Word length bounds
+#define MIN_WORD_LENGTH 1   ///< Length of the smallest word
+#define MAX_WORD_LENGTH 16  ///< Length of the longest word.
+
+// Word level range
+#define MIN_LEVEL 1         ///< The minimum word level.
+#define MAX_LEVEL 100       ///< The maximum word level.
+
+// Range for word stats
+#define MIN_BASE_STAT 1     ///< The minimum base stat value.
+#define MAX_BASE_STAT 255   ///< The maximum base stat value.
+
+// Range for actual word stats
+#define MIN_STAT 1          ///< The minimum value for any computed stat.
+#define MAX_STAT 9999       ///< The maximum value for any computed stat.
+
+/**********************************************************//**
+ * @enum STAT
+ * @brief Defines all word statistics.
+ **************************************************************/
+typedef enum {
+    MAXHP,  ///< Maximum health.
+    ATTACK, ///< Attack power.
+    DEFEND, ///< Defensive power.
+    SPEED,  ///< Speed and agility.
+} STAT;
+
+/// The total number of unique stats.
+#define N_STATS (SPEED+1)
+
+/**********************************************************//**
+ * @struct WORD
+ * @brief Defines all the data in one word.
+ **************************************************************/
+typedef struct {
+    // Constant properties
+    char text[MAX_WORD_LENGTH+1];   ///< Actual text of the word
+    int base[N_STATS];              ///< Constant base stats.
+    TECHNIQUE techs[MAX_TECHNIQUES];///< Techniques known.
+    int nTechs; ///< Number of techniques.
+    int rank;   ///< Rank of the word.
+    int bounty; ///< Experience this word is worth.
+    
+    // Current stats (recalculate on level up)
+    int level;          ///< Level of the word
+    int hp;             ///< Current HP
+    int exp;            ///< Current EXP
+    int stat[N_STATS];  ///< Current stats
+    
+} WORD;
+
+/**********************************************************//**
+ * @brief Create a word
+ * @param word: Output parameter for the word which is being
+ * constructed.
+ * @param text: The English text of the word.
+ * @param level: Initial level of the word
+ * @return Whether the word was successfully constructed.
+ **************************************************************/
+extern bool word_Create(WORD *word, const char *text, int level);
+
+/**********************************************************//**
+ * @brief Heal or damage the word
+ * @param word: The word to read
+ * @param delta: heal if > 0 otherwise damage
+ **************************************************************/
+extern void word_ChangeCurrentHP(WORD *word, int delta);
+
+/**********************************************************//**
+ * @brief Increase the EXP of the word and level up if needed
+ * @param word: The word in question
+ * @param delta: the amount of EXP earned
+ **************************************************************/
+extern void word_ChangeExperience(WORD *word, int delta);
+
+/*============================================================*/
+#endif // _WORD_H_
