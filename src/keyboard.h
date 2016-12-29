4b206e0cd947476f62adfbf06557ee59a54707d4
Rena <renashinomiya@gmail.com>
2016-12-28 21:01:55 -0600

    Convert keyboard keys to an enum

--- src/keyboard.h
+++ src/keyboard.h
@@ -10,33 +10,20 @@
 // Allegro
 #include <allegro5/allegro.h>
 
-/*============================================================*
- * Key macros
- *============================================================*/
-
-/// The up key
-#define KEY_UP ALLEGRO_KEY_UP
-
-/// The down key
-#define KEY_DOWN ALLEGRO_KEY_DOWN
-
-/// The left key
-#define KEY_LEFT ALLEGRO_KEY_LEFT
-
-/// The right key
-#define KEY_RIGHT ALLEGRO_KEY_RIGHT
-
-/// The "Start" key
-#define KEY_START ALLEGRO_KEY_W
-
-/// The "Select" key
-#define KEY_SELECT ALLEGRO_KEY_A
-
-/// The "A" key
-#define KEY_A ALLEGRO_KEY_D
-
-/// The "B" key
-#define KEY_B ALLEGRO_KEY_S
+/**********************************************************//**
+ * @enum KEY
+ * @brief All the keys used for user input.
+ **************************************************************/
+typedef enum {
+    KEY_UP=ALLEGRO_KEY_UP,          ///< Moves up.
+    KEY_DOWN=ALLEGRO_KEY_DOWN,      ///< Moves down.
+    KEY_LEFT=ALLEGRO_KEY_LEFT,      ///< Moves left.
+    KEY_RIGHT=ALLEGRO_KEY_RIGHT,    ///< Moves right.
+    KEY_START=ALLEGRO_KEY_W,        ///< Start key (begin, menu).
+    KEY_SELECT=ALLEGRO_KEY_A,       ///< Select key (special).
+    KEY_A=ALLEGRO_KEY_D,            ///< Confirm a choice.
+    KEY_B=ALLEGRO_KEY_S,            ///< Deny a choice.
+} KEY;
 
 /*============================================================*/
 #endif // _KEYBOARD_H_
