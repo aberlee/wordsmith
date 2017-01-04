154f1c51be9f5dc0cbbe03ea024f7e2cf7157a12
Rena <renashinomiya@gmail.com>
2017-01-03 21:48:15 -0600

    Remove undershading in bars

--- src/bar.c
+++ src/bar.c
@@ -23,21 +23,12 @@ void bar_Draw(const BAR *bar) {
     
     // Ratio variables
     int xr = bar->x + bar->ratio*bar->width;
-    int ym = (yo + yf) / 2;
     
     // The background of the entire bar
     al_draw_filled_rectangle(xo, yo, xf, yf, bar->background);
     
-    // Upper highlight of the bar
-    al_draw_filled_rectangle(xo, yo, xr, ym, bar->foreground);
-    
-    // Lower underlay of the bar (use middle color)
-    ALLEGRO_COLOR middleground;
-    unsigned char ar, ag, ab, br, bg, bb;
-    al_unmap_rgb(bar->foreground, &ar, &ag, &ab);
-    al_unmap_rgb(bar->background, &br, &bg, &bb);
-    middleground = al_map_rgb((ar+br) / 2, (ag+bg) / 2, (ab+bb) / 2);
-    al_draw_filled_rectangle(xo, ym, xr, yf, middleground);
+    // Filled section
+    al_draw_filled_rectangle(xo, yo, xr, yf, bar->foreground);
 }
 
 /*============================================================*/
